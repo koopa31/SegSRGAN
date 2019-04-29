e06982681defc938ebf76e5b70e9d61a5345e7b0
Clément Cazorla <clement.cazorla@univ-reims.fr>
2019-04-29 12:09:45 +0200

    modification architecture

--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
 import setuptools
 from os.path import abspath
+import wget
+from setuptools.command.install import install
 
-with open(abspath("deepBrain-master/README.md"), "r") as fh:
+with open(abspath("SegSRGAN/README.md"), "r") as fh:
     long_description = fh.read()
 
-with open(abspath("deepBrain-master/requirements.txt")) as f:
+with open(abspath("SegSRGAN/requirements.txt")) as f:
     requirements = f.read().splitlines()
+
+class PostInstallCommand(install):
+	def run(self):
+		url = 'http://www.futurecrew.com/skaven/song_files/mp3/razorback.mp3'
+		wget.download(url)
+		install.run(self)
 
 setuptools.setup(
     name="SegSRGAN",
-    version="1.0",
+    version="1.0.12",
     author="Clément Cazorla",
     author_email="clement.cazorla@univ-reims.fr",
     description="Segmentation and super resolution GAN network",
@@ -18,4 +26,6 @@ setuptools.setup(
     url="https://github.com/koopa31/SegSRGAN/tree/develop",
     packages=setuptools.find_packages(),
     install_requires=requirements,
+    scripts=['script.py'],
 )
+#cmdclass={'install': PostInstallCommand}
