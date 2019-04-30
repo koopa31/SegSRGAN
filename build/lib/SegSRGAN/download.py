c1cf1b762b26896cfc0c38313c0383cf675c4658
Clément Cazorla <clement.cazorla@univ-reims.fr>
2019-04-30 14:01:39 +0200

    package pip fonctionnel

---
+++ build/lib/SegSRGAN/download.py
@@ -0,0 +1,19 @@
+import os
+
+import wget
+import requests
+
+
+def download_weights():
+    # We get the content of the weights folder
+    z = requests.get('https://api.github.com/repos/koopa31/SegSRGAN/contents/SegSRGAN/SegSRGAN/weights?ref=develop')
+    contents = z.json()
+    weights_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'SegSRGAN/weights')
+    # Creation of weights folder if it does not already exist
+    if os.path.isdir(weights_path) is False:
+        os.mkdir(weights_path)
+    # Downloading of the files
+    for content in contents:
+        if os.path.isfile(os.path.join(weights_path, content['name'])) is False:
+            print("Processing %s" % content['path'])
+            wget.download(content['download_url'], out=weights_path)
--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 import setuptools
 from os.path import abspath
-import wget
-from setuptools.command.install import install
+
 
 with open(abspath("SegSRGAN/README.md"), "r") as fh:
     long_description = fh.read()
@@ -9,15 +8,10 @@ with open(abspath("SegSRGAN/README.md"), "r") as fh:
 with open(abspath("SegSRGAN/requirements.txt")) as f:
     requirements = f.read().splitlines()
 
-class PostInstallCommand(install):
-	def run(self):
-		url = 'http://www.futurecrew.com/skaven/song_files/mp3/razorback.mp3'
-		wget.download(url)
-		install.run(self)
 
 setuptools.setup(
     name="SegSRGAN",
-    version="1.0.12",
+    version="1.0.16",
     author="Clément Cazorla",
     author_email="clement.cazorla@univ-reims.fr",
     description="Segmentation and super resolution GAN network",
@@ -26,6 +20,4 @@ setuptools.setup(
     url="https://github.com/koopa31/SegSRGAN/tree/develop",
     packages=setuptools.find_packages(),
     install_requires=requirements,
-    scripts=['script.py'],
 )
-#cmdclass={'install': PostInstallCommand}
