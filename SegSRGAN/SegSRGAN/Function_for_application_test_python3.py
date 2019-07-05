3b8a170666b7a506c267428f4bada27adf8c60e7
Quentin <quentin.delannoy94@gmail.com>
2019-07-05 14:35:56 +0200

    corection erreur concernant les exception step > patch et patch > taille_image

--- SegSRGAN/SegSRGAN/Function_for_application_test_python3.py
+++ SegSRGAN/SegSRGAN/Function_for_application_test_python3.py
@@ -317,11 +317,11 @@ def segmentation(input_file_path, step, new_resolution, path_output_cortex, path
         patch2 = width
         patch3 = depth
     
-    if ((step>patch1) |  (step>patch2) | (step>patch1)) & patch is not None :
+    if ((step>patch1) |  (step>patch2) | (step>patch3)) & (patch is not None) :
         
         raise AssertionError('The step need to be smaller than the patch size')
         
-    if (np.shape(padded_interpolated_image)[0]<patch1)|(np.shape(padded_interpolated_image)[1]<patch2)|(np.shape(padded_interpolated_image)[3]<patch3):
+    if (np.shape(padded_interpolated_image)[0]<patch1)|(np.shape(padded_interpolated_image)[1]<patch2)|(np.shape(padded_interpolated_image)[2]<patch3):
         
         raise AssertionError('The patch size need to be smaller than the interpolated image size')
 
